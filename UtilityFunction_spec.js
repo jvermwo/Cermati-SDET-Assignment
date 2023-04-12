6ba16dfbf633f5f03fa12ff859cbae98a2e026c2
jvermwo <130583196+jvermwo@users.noreply.github.com>
2023-04-12 21:05:22 +0530

    Create UtilityFunction_spec.js

---
+++ UtilityFunction_spec.js
@@ -0,0 +1,10 @@
+const supertest = require("supertest")
+const config = require('./config').config;
+const cardBaseUrl = supertest(config.cardBaseUrl);
+
+var UtilityFunction = function () {
+
+    
+       
+}
+module.exports = new UtilityFunction();
